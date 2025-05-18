2fb20c0486c012ca0a4b52460cb25c8bdec33dff
Nazar Bakay <bakayn484@gmail.com>
2025-05-19 00:17:01 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,23 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    // write code here
+    const elementDiv = document.createElement("div");
+    elementDiv.classList.add("notification", type);
+    elementDiv.style.top = posTop + "px";
+    elementDiv.style.right = posRight + "px";
+    const header = document.createElement("h2");
+    header.classList.add("title");
+    header.textContent = title;
+    const paragraph = document.createElement("p");
+    paragraph.textContent = description;
+    elementDiv.append(header, paragraph);
+    document.body.appendChild(elementDiv);
+    setTimeout(()=>{
+        elementDiv.style.display = "none";
+    }, 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
